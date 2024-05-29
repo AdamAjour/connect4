d5212a40dbbb28c57370e233387abd4b400a055a
Adam Ajour <adamajour2004@gmail.com>
2024-05-30 02:16:42 +0300

    Alternate between two players

--- connect4.rb
+++ connect4.rb
@@ -1,9 +1,15 @@
 require 'rainbow/refinement'
+require 'securerandom'
 using Rainbow
 
 Rows = 6
 Columns = 7
-Coin = ["heads" , "tails"]
+
+Player1 = ""
+Player2 = ""
+Coin = ["", ""]
+CurrentPlayer = ""
+
 GameOver = false
 LetterNum = {
   "A" => 0,
@@ -15,9 +21,16 @@ LetterNum = {
   "G" => 6
 }
 
-red_piece = "█".red
-yellow_piece = "█".blue
-empty_slot = "█"
+RedPiece = "█".red
+GreenPiece = "█".green
+PieceArray = [RedPiece, GreenPiece]
+EmptySlot = "█"
+Index = 0
+
+
+Flip = SecureRandom.random_number(2)
+
+
 
 Table = Array.new(Rows) { |i| Array.new(Columns) { |i| "█" }}
 
@@ -25,6 +38,49 @@ Table = Array.new(Rows) { |i| Array.new(Columns) { |i| "█" }}
 def nl()
   print("\n")
 end
+
+def players_name()
+  print("Enter your name first to choose heads\n")
+  print("Player 1 enter your name: ")
+  @Player1 = gets.chomp
+  print("Player 2 enter your name: ")
+  @Player2 = gets.chomp
+  @Coin = [@Player1 , @Player2]
+  @CurrentPlayer = @Coin[Flip]
+end
+
+def player_banner(current_player)
+  borders()
+  empty_row_no_columns()
+  players_turn_banner(current_player)
+  empty_row_no_columns()
+end
+
+def switch_player()
+  @Flip = (@Flip.to_i + 1) % 2
+  @Index = (@Index.to_i + 1) % 2
+  @CurrentPlayer = @Coin[@Flip]
+end
+
+def empty_row_no_columns()
+  print("¦")
+  for space_row in 0..6 do
+    print("\t\t") 
+  end
+  print("¦")
+  nl()
+end
+
+def players_turn_banner(current_player)
+
+  for space_row in 0..2 do
+    print("\t\t") 
+  end
+  print("#{current_player}'s Turn")
+
+  nl()
+end
+
 
 def empty_row()
   for space_row in 0..6 do
@@ -79,10 +135,30 @@ end
 def take_input()
   print("Insert in Column : ")
   letter = gets.chomp
+  is_column_full = insert_piece(PieceArray[@Index.to_i],letter.capitalize)
 end
 
-def insert_piece(column)
-
+def insert_piece(piece, column)
+  column_number = LetterNum[column]
+  full = false
+  for slot in (Rows-1).downto(0) do
+    if Table[slot][column_number] == EmptySlot
+      Table[slot][column_number] = piece
+      break
+    end
+  end
+  if(Table[0][column_number] != EmptySlot)
+    full = true
+  end
+  return full
 end
-take_input()
-initialBoard()
+
+
+players_name()
+nl()
+while(!GameOver)
+  player_banner(@CurrentPlayer)
+  initialBoard()
+  take_input()
+  switch_player()
+end
